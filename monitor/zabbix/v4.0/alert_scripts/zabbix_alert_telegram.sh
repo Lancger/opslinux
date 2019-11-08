7351c40c2cee6c9249cf10e8bf11686cf4236604
Lancger <1151980610@qq.com>
2019-11-08 22:58:40 +0800

    Create zabbix_alert_telegram.sh

---
+++ monitor/zabbix/v4.0/alert_scripts/zabbix_alert_telegram.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import requests
+import json
+import sys
+
+TOKEN="904193186:AAFan97mHMyZJUXR1hmynWt4nq7j1Y7YX4k"
+
+def Send_Message(chat_id, Content):
+    url="https://api.telegram.org/bot${TOKEN}/sendMessage"
+    data = {
+            "chat_id": chat_id,
+            "text": Content, 
+            }
+    res = requests.post(url,json=data)
+    return res.text
+
+
+
+if __name__ == '__main__':
+    chat_id = sys.argv[1]
+    Content = sys.argv[2]
+
+    Send_Message(chat_id, Content)
