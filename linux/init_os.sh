0af5fac35dcd7ffb48508297018d107ee1973e56
Lancger <1151980610@qq.com>
2023-09-13 18:58:38 +0800

    Update init_os.sh

--- linux/init_os.sh
+++ linux/init_os.sh
@@ -61,7 +61,7 @@ function initialization_check() {
 #配置yum源
 function yum_config(){
     yum makecache
-    yum -y install vim yum-utils wget telnet bind-utils epel-release rsync bc lsof traceroute strace net-snmp lrzsz zip xz unzip vnstat iotop iftop net-tools openssh-clients gcc gcc-c++ make cmake libxml2-devel openssl-devel curl curl-devel sudo ntp ntpdate ncurses-devel autoconf automake zlib-devel python-devel iptables-services iptables psmisc pcre* bash-completion
+    yum -y install vim tree yum-utils wget telnet bind-utils epel-release rsync bc lsof traceroute strace net-snmp lrzsz zip xz unzip vnstat iotop iftop net-tools openssh-clients gcc gcc-c++ make cmake libxml2-devel openssl-devel curl curl-devel sudo ntp ntpdate ncurses-devel autoconf automake zlib-devel python-devel iptables-services iptables psmisc pcre* bash-completion
 }
 
 #firewalld
